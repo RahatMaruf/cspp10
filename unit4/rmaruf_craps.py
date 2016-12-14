cc11ef345939ca2f2ca72b55329a9c0c78ea2485
Rahat <rahatmaruf@uagateway.org>
2016-12-14 17:15:56 +0000

    worked on craps

--- unit4/rmaruf_craps.py
+++ unit4/rmaruf_craps.py
@@ -14,7 +14,7 @@ bank = 10000000000000000000000
 #write a function that asks the user for their bet and takes away the amount they bet from their bank
 
 def bet():
-    dabet = int(input("How much would you ike to bet from your bank"))
+    dabet = int(input("You have $100 in your bank. How much would you like to bet from your bank"))
     return (dabet)
 
 #make a function that let"s the computer pick out two number betwwn 1 and 6
@@ -23,34 +23,24 @@ def roll2dice():
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
     return dice1 + dice2
+    point_number = dice1 + dice2
+    
+
     
     
-def loss_or_win():
+def loss_or_win(dice1, dice2, dabet, playersbank):
     
     if dice1 + dice2 == 2 or dice1 + dice2 == 3 or dice1 + dice2 == 12:
         return ("You lost. You now have $" + str(dabet - playersbank))
     elif dice1 + dice2 == 7 or dice1 + dice2 == 11:
         return ("You win. You have $" + str(dabet + playersbank))
         
-def get_phase3(point_number):
-    roll = roll2dice()
-    while(roll != 7 and roll != point_number):
+# def get_phase3(point_number):
+#     roll = roll2dice()
+#     while(roll != 7 and roll != point_number):
         
         
         
-
-
-
-
-
-#OPTION 2
-# function name: roll_result
-#   purpose: get the result of the roll
-#   arguments: 
-#       roll - the sum of the two dice rolled
-#       is_first_roll - true/false depending on which roll it is
-#   returns: the result
-#       if player wins: return "win"
-#       if player lost: return "lose"
-
-
+        
+        
+        
