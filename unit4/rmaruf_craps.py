fafd6d1d003c38057107bf1617cd914df4de7c5c
Rahat <rahatmaruf@uagateway.org>
2016-12-20 17:12:27 +0000

    worked on craps

--- unit4/rmaruf_craps.py
+++ unit4/rmaruf_craps.py
@@ -8,16 +8,17 @@ import random
 
 playersbank = 100
 
-#the bank has an unlimited amount of money 
 
-bank = 10000000000000000000000
 
 #write a function that asks the user for their bet and takes away the amount they bet from their bank
 
 def bet():
     dabet = int(input("You have $100 in your bank. How much would you like to bet from your bank"))
-    return (dabet)
-    if dabet
+    if dabet < 0:
+        return ("Only positive amount of money!")
+    else:
+        return (dabet)
+bet()
 
 #make a function that let"s the computer pick out two number betwwn 1 and 6
 
@@ -26,7 +27,7 @@ def roll2dice():
     dice2 = random.randint(1,6)
     point_number = dice1 + dice2
     return point_number
-    
+roll2dice()
     
 
     
@@ -39,17 +40,20 @@ def loss_or_win(dice1, dice2, dabet, playersbank, point_number):
         return ("You win. You have $" + str(dabet + playersbank))
     else:
         return (point_number)
+loss_or_win()
             
             
 def get_phase3(point_number, dabet, playersbank):
         roll = roll2dice()
         while(roll != 7 and roll != point_number):
             return roll
-            if roll == 7:
-                return ("You lose! You now have $" + str(dabet - playersbank))
-            elif roll == point_number:
-                return ("You Win! You now have $" + str(dabet + playersbank))
-                
+            
+        if roll == 7:
+            return ("You lose! You now have $" + str(dabet - playersbank))
+        elif roll == point_number:
+            return ("You Win! You now have $" + str(dabet + playersbank))
+
+get_phase3()       
                 
         
         
