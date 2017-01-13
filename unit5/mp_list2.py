d6142e884d24f4392e620646355f074cfedf7de7
Rahat <rahatmaruf@uagateway.org>
2017-01-13 17:15:31 +0000

    worked on lists2

---
+++ unit5/mp_list2.py
@@ -0,0 +1,6 @@
+numero = []
+actions = ("Nothing")
+while actions != "Exit":
+    users_input_choice = int(input("Choose a number. "))
+    users_input_action = input("What would you like to do with the list? ")
+    if actions >=
