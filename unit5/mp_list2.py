60c8d3a762ac5a78809af6b9710e59f8ec5d08c3
Rahat <rahatmaruf@uagateway.org>
2017-01-20 17:15:22 +0000

    worked on lists3

--- unit5/mp_list2.py
+++ unit5/mp_list2.py
@@ -1,6 +1,24 @@
 numero = []
-actions = ("Nothing")
-while actions != "Exit":
+actions = input("What would you like to do with the list? ").lower
+while actions != "exit":
     users_input_choice = int(input("Choose a number. "))
-    users_input_action = input("What would you like to do with the list? ")
-    if actions >=
+    
+    if users_input_choice >= 0:
+        numero.insert(0, users_input_choice)
+    
+    elif actions == "sum":
+        sum(numero)
+    
+    elif actions == "clear":
+         numero.remove(users_input_choice)
+
+    elif actions == "print":
+        print (numero)
+        
+    elif actions == "length":
+        print (len(numero))
+
+if actions == "exit":
+    print ("exited")
+    
+        
