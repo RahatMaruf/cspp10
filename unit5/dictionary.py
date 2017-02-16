710e957145509f9b9954b5069ffff66cd947a7aa
Rahat <rahatmaruf@uagateway.org>
2017-02-16 17:13:25 +0000

    continued on the dictionary thing started from yesterday adding a couple things to make the add value to work

---
+++ unit5/dictionary.py
@@ -0,0 +1,7 @@
+d = {}
+choose = input("Add key-value\nRemove Key\nRemove Value\nUpdate\nExit\n choose one. ").lower
+if choose == ("add key-value"):
+    key = d[input("Enter a key. ")]
+    value = d[input("Enter a value. ")]
+    
+    
